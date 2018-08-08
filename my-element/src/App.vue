2a2aafd12387bd358083029116da144ef57c198b
Moocliu <18757132070@163.com>
2018-08-08 17:11:19 +0800

    this is a simple system

---
+++ my-element/src/App.vue
@@ -0,0 +1,21 @@
+
+<template>
+<div id="app">
+  <el-radio v-model="radio" label="1">备选项</el-radio>
+  <el-radio v-model="radio" label="2">备选项</el-radio>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name:'APP',
+    data () {
+      return {
+        radio: '1'
+      };
+    }
+  }
+</script>
+
+
